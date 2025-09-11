9d3e11cdc935bec10ff23c3318f6a4cf483a2e4f
jimit8929 <jimit8929@gmail.com>
2025-09-11 19:29:55 +0530

    order model

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 import userRoutes from "./routes/user.route.js";
 import bookRoutes from "./routes/book.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -19,7 +20,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,7 +41,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/books", bookRoutes);
-app.use("/api/cart" , cartRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
